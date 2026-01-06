a44721a66d06d4d94145703f7cac65118d2fc5cd
MatthewLizzard <161202393+MatthewLizzard@users.noreply.github.com>
2026-01-06 16:31:30 +1100

    fixed main.go

--- src/fountain/main.go
+++ src/fountain/main.go
@@ -1,50 +1,66 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "log"
-    "os"
-    "os/exec"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("oh noes")
-        return
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("usage: fountain <file>")
+		return
+	}
 
-    filename := os.Args[1]
-    fmt.Println("script:", filename)
-    fmt.Println("-----------")
-    fmt.Println("")
+	filename := os.Args[1]
 
-    // execute via bash (will change to execute via own shell)
-    cmd := exec.Command("bash", filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
+	defer file.Close()
 
-    // stdout pipe
-    stdout, err := cmd.StdoutPipe()
-    if err != nil {
-        log.Fatal(err)
-    }
+	reader := bufio.NewReader(file)
+	firstline, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
 
-    // stderr pipe
-    stderr, err := cmd.StderrPipe()
-    if err != nil {
-        log.Fatal(err)
-    }
+	if strings.HasPrefix(firstline, "#!") {
+		shebang := strings.TrimSpace(firstline[2:])
+		var cmd *exec.Cmd
 
-    // start the command
-    if err := cmd.Start(); err != nil {
-        log.Fatal(err)
-    }
+		switch shebang {
+		case "/usr/local/bin/fountain":
+			cmd = exec.Command("./core/f1", filename)
 
-    // stream output
-    go io.Copy(os.Stdout, stdout)
-    go io.Copy(os.Stderr, stderr)
+			// changing main process and exiting launcher
+			args := []string{"./core/f1", filename}
+			syscall.Exec("./core/f1", args, os.Environ())
 
-    // wait for command to finish
-    if err := cmd.Wait(); err != nil {
-        log.Fatal(err)
-    }
+		case "/usr/local/bin/ff":
+			cmd = exec.Command("./core/f1", filename)
+
+			// changing main process and exiting launcher
+			args := []string{"./core/f1", filename}
+			syscall.Exec("./core/f1", args, os.Environ())
+
+		default:
+			cmd = exec.Command(shebang, filename)
+		}
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			fmt.Println("error")
+		}
+	} else {
+		fmt.Println("error")
+	}
 }
